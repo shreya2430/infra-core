96065123647d9321a54903a2c0e7ae820bd7856e
Shreya Wanisha <wanisha.s@northeastern.edu>
2025-10-09 12:35:59 -0400

    Refactor subnet resource definitions to use for_each instead of count

--- outputs.tf
+++ outputs.tf
@@ -10,12 +10,12 @@ output "vpc_cidr" {
 
 output "public_subnet_ids" {
   description = "IDs of public subnets"
-  value       = aws_subnet.public[*].id
+  value       = values(aws_subnet.public)[*].id
 }
 
 output "private_subnet_ids" {
   description = "IDs of private subnets"
-  value       = aws_subnet.private[*].id
+  value       = values(aws_subnet.private)[*].id
 }
 
 output "internet_gateway_id" {
